3b15b8977157e4373d340918ce34379505645af4
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-09-30 22:08:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const makePizza = function (){
+ console.log("deeg kneden");
+ console.log("topping verdelen");
+ console.log("pizza in de oven");
+ };
+
+ //krijg geen resultaat met alleen de bovenstaande want const makePizza is een functie geworden
+
+
+ const makeSushi = function(){
+ console.log("kleefrijst maken");
+ console.log("rolletjes maken");
+ console.log("stukjes snijden");
+ };
+
+ //3x sushi en 2x pizza
+ makeSushi();
+ makeSushi();
+ makeSushi();
+ makePizza();
+ makePizza();
